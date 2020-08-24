56ff7ca8721a07461ba69239cc985feda47e49f4
Ryan DiSalle <ryandisalle@yahoo.com>
2020-08-24 18:13:03 -0400

    moreupdates

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DB_URL || 'postgres://awbqezlkeqflrp:04bf7d41c3aa4863e0d561dcf43801a7544585bef7ef2c188def4ab7d3cc9210@ec2-54-156-121-142.compute-1.amazonaws.com:5432/d41mg672cagfv0',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful-test',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful-test'
 
   }
