e170939d1c02ce05b2bd173a9962d52a3ab57ca1
Ryan DiSalle <ryandisalle@yahoo.com>
2020-08-25 20:03:15 -0400

    update

--- src/notes/notes-router.js
+++ src/notes/notes-router.js
@@ -68,7 +68,7 @@ notesRouter
   .get((req, res, next) => {
     res.json(serializeNote(res.note))
   })
-  .delete(jsonParser, (req, res, next) => {
+  .delete((req, res, next) => {
     NotesService.deleteNote(
       req.app.get('db'),
       req.params.note_id
